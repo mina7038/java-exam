ac85d81f7939cd713d8e9335d2bb24fbc71378ef
mina7038 <alsk4325@naver.com>
2025-02-25 20:00:00 +0900

    반복문

---
+++ Day02/ForWhile.java
@@ -0,0 +1,12 @@
+public class ForWhile {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("for 반복: " + i);
+        }
+        int j = 1;
+        while (j <= 5) {
+            System.out.println("while 반복: " + j);
+            j++;
+        }
+    }
+}
