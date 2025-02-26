71bc5b56c1e4361f801032fa2c97f5ccc88e9c50
mina7038 <alsk4325@naver.com>
2025-02-26 20:00:00 +0900

    배열

---
+++ Day03/ArrayTest.java
@@ -0,0 +1,8 @@
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30};
+        for (int n : nums) {
+            System.out.println("배열 값: " + n);
+        }
+    }
+}
